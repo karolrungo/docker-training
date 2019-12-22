af93bff99ff57b7c77317149e97420e917277e75
rungokarol <karolrungo@gmail.com>
2019-12-22 18:27:13 +0100

    changed text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn react
+          This text has been changed
         </a>
       </header>
     </div>
